50c2e6525fa93edde433d3cef0ec32c70c120453
Moshood Sanusi <sanusimoshoodabiola@gmail.com>
2022-11-08 00:00:15 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import React from "react";
+import AppCounter from "./AppCounter";
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppCounter />
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/AppCounter.jsx
@@ -0,0 +1,31 @@
+//  import React from 'react'
+import { useState } from 'react'
+import "./index.css";
+
+function AppCounter() {
+
+    const initial = 0;
+    const[value, setValue] = useState(initial);
+
+    const handleIncrement = () => {
+        setValue(value + 1);
+    }
+    const handleDecrement = () => {
+        setValue(value - 1);
+    }
+    // const handleReset = () => {
+    //     {initial}
+    // }
+
+
+  return (
+    <div className='container'>
+        <h2>{value}</h2>
+        <button className = "btn" onClick = {handleIncrement}>Increment</button>
+        <button  className = "btn" onClick = {handleDecrement}>Decrement</button>
+        <button className = "btn"  onClick = {initial}>Reset</button>
+    </div>
+  )
+}
+
+export default AppCounter;
